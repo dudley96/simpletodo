4933ca763e25011f350cc60de9015ac7178a38bc
unknown <yapdudley@gmail.com>
2021-03-22 23:53:11 +0800

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+	<p>lorem isum</p>
+	<p>lorem ipsum</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
